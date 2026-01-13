7409ade9ab963424eedca5e4ee0789997d8c43e1
cgeach-dlr <christopher.geach@dlr.de>
2026-01-13 14:15:53 +0100

    Add files via upload

---
+++ Sodium iron and helium/generate_helium_saturation_plots.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append("")
+import helium_saturation_calculator_library as he_lib
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+font = {'weight' : 'normal',
+        'size'   : 20}
+
+matplotlib.rc('font', **font)
+
+#Designate an output location for figure data and plots
+outpath_data = 'C:/Users/geac_ch/Documents/HELIX/Saturation paper/Fig_data/temp/'
+outpath_figs = 'C:/Users/geac_ch/Documents/HELIX/Saturation paper/Figs/temp/'
+
+#Calculates the spectrum of the degree of saturation.
+
+nu_Ls = (np.arange(59) - 29)*4e8 #Hz
+sats_vdG_nu_Ls = np.zeros(len(nu_Ls))
+sats_Megie_nu_Ls = np.zeros(len(nu_Ls))
+
+E_pulse = 4.7 #mJ
+t_L = 200 #ns
+T_atm = 0.9
+z = 500000 #km
+alpha_L = 30e-6 #radians
+alpha_T = 61e-6 #radians
+delta_r = 31e-6 #radians
+
+nt = 200
+delta_t = 5 #ns
+
+N_L = he_lib.N_L_from_pulse_energy(E_pulse)
+Delta_nu_L = 20e6 #Hz
+lineshape = 'gauss'
+Temp_He = 1000 #K
+Doppler_spectrum = he_lib.get_total_scattering_cross_section_spectrum(Temp_He)
+
+for i in range(len(nu_Ls)):
+    print(i)
+    nu_L = nu_Ls[i]    
+    sats_vdG_nu_Ls[i] = he_lib.get_saturation_beam(nu_L, Delta_nu_L, N_L, z,
+                          T_atm, alpha_L, alpha_T, t_L, nt, delta_t, delta_r,
+                          Temp_He, lineshape, ratio_beam=True)    
+    g_L = he_lib.get_laser_pulseshape(nu_L, Delta_nu_L, lineshape)
+    sigma_eff = np.sum(g_L * Doppler_spectrum) / np.sum(g_L)
+    sats_Megie_nu_Ls[i] = he_lib.get_saturation_megie(z, alpha_L, t_L,
+                          sigma_eff, N_L, T_atm)
+
+
+#Calculates the saturation-induced density, temperature, and wind biases as a
+# function of laser pulse energy
+
+delta_ts = np.array([10, 10, 8, 5, 3.2, 2, 1.25, 0.8, 0.5, 0.32, 0.2, 0.125])
+nts = 1000 / delta_ts
+
+nu_Ls_errs = np.array([-3.58e9, 0, 2.04e9, 3.32e9])
+
+Es = 10**np.arange(0, 2.1, 0.2) #mJ
+Es = np.hstack((1e-3, Es))
+
+dens_err_200 = np.zeros(len(Es)) #Arbitrary units
+T_err_200 = np.zeros(len(Es)) #K
+w_err_200 = np.zeros(len(Es)) #m/s
+
+for i in range(len(Es)):
+    print(i)
+    N_L = he_lib.N_L_from_pulse_energy(Es[i])
+    Res = he_lib.get_wind_and_temp_errors(Temp_He, nu_Ls_errs, Delta_nu_L,
+                                          N_L, z, T_atm, alpha_L, alpha_T,
+                                          t_L, nts[i], delta_ts[i], delta_r,
+                                          lineshape)
+      
+    dens_err_200[i] = (Res[0][0][0] - Res[1][0][0])/Res[1][0][0]
+    T_err_200[i] = Res[0][0][1] - Res[1][0][1]
+    w_err_200[i] = Res[0][0][2] - Res[1][0][2]  
+
+fig,ax = plt.subplots(1,2, figsize=(16,8))
+
+ax[0].plot(nu_Ls*1e-9, 100*sats_vdG_nu_Ls, label='von der Gathen approach')
+ax[0].plot(nu_Ls*1e-9, 100*sats_Megie_nu_Ls,
+           label=r'Megie approach ($\sigma_{\mathrm{t}}$)')
+
+ax[0].text(.06,.92, '(a)', transform=plt.gcf().transFigure)
+ax[0].set_ylabel('Saturation percent')
+ax[0].set_xlabel('Central laser frequency(GHz)')
+ax[0].set_ylim(-1,34)
+ax[0].legend()
+ax[0].grid(True)
+
+ax[1].text(.51,.93, '(b)', transform=plt.gcf().transFigure)
+ax[1].plot(Es, 100*dens_err_200, label = 'Density error')
+ax[1].plot(Es, T_err_200, label = 'Temperature error')
+ax[1].plot(Es, w_err_200, label = 'Wind error')
+
+ax[1].set_xlabel('Laser pulse energy (mJ)')
+ax[1].set_ylabel('Percent density error \n Absolute temperature error (K)\n' +
+                 'Absolute wind error (m/s)')
+ax[1].legend()
+ax[1].grid(True)
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+fig.tight_layout()
+
+plt.savefig(os.path.join(outpath_figs, 'He.pdf'), dpi=300)
+
+He_spectrum_data = np.vstack((nu_Ls*1e-9, 100*sats_vdG_nu_Ls, 
+                              100*sats_Megie_nu_Ls))
+np.savetxt(os.path.join(outpath_data, 'He_saturation.txt'),
+           He_spectrum_data.T, delimiter=',')
+
+He_temp_and_wind_biases_data = np.vstack((Es, 100*dens_err_200, T_err_200,
+                                          w_err_200))
+np.savetxt(os.path.join(outpath_data, 'He_temp_and_wind_biases.txt'),
+           He_temp_and_wind_biases_data.T, delimiter=',')
---
+++ Sodium iron and helium/helium_saturation_calculator_library.py
@@ -0,0 +1,314 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import scipy.optimize as opt
+import scipy.interpolate as si
+
+c_light = 2.99792458 * 10**8 #m/s
+k_B = 1.38064852 * 10**-23 #J K^-1
+h_planck = 6.626*10**-34
+N_A = 6.022 * 10**23 #mol^-1
+e_elec = 1.60218*10**-19 #C
+eps_0 = 8.85419*10**-12 # F/m
+m_elec = 9.10938*10**-31 #kg
+M_He = 0.004003 #kg mol^-1
+m_He = M_He/N_A
+
+lamb0 =  1083.3306444 * 10**-9 #m, in vacuum
+nu0 = c_light / lamb0
+A_i = 10.22e6 #Hz
+tau_R = 1 / A_i * 1e9 #ns
+Delta_nu_n = A_i / (2 * np.pi) #Hz
+absorb_coeff = e_elec**2/(4*eps_0 * m_elec * c_light)
+sig_D = nu0*np.sqrt(k_B / (m_He * c_light**2))
+f_D = 0.539
+
+g1_0 = 1
+g1_1 = 3
+g1_2 = 5
+sum_g = 9
+
+#Relative frequency shift
+nu_11 = 2.29116 * 10**9 #Hz
+nu_12 = 0 #Hz
+
+#Only transitions 1_1 and 1_2 are considered.
+g_jk = np.array([g1_1, g1_2])
+nu_jk = np.array([nu_11, nu_12])
+
+#Hanle factors - assumed to be 1 here.
+q_jk = np.array([1, 1]) 
+
+delta_nu = 3e5
+nu_shifts = np.arange(-12*10**9, 12*10**9, delta_nu)
+nv = len(nu_shifts)
+
+def convolve(a,b):
+    #Calculates the convolution of two arrays and corrects for the shift in
+    # index arising from np.convolve
+    conv = np.convolve(a,b,'same')*delta_nu
+    conv[:-1] = conv[1:]
+    return conv
+
+def get_natural_absorption_line(k):
+   #Returns the scattering cross-section spectrum of the transition
+   # to the kth excited state (natural linewidth only)
+ 
+    #Need to make sure sufficient resolution is used to resolve the absorption
+    # line
+    if nu_shifts[1]-nu_shifts[0] > Delta_nu_n / 5.:
+        print('Error: insufficient spectral resolution to resolve' + 
+              ' absorption line.')
+        return
+        
+    natural_absorption_line = (g_jk[k] / sum_g * absorb_coeff * f_D / np.pi * 
+                       Delta_nu_n / 2 / ((nu_shifts - nu_jk[k])**2
+                                         + (Delta_nu_n/2)**2))
+    return natural_absorption_line
+
+def get_combined_absorption_line():
+    #Returns the combined absorption line for both 2^3S <--> 2^3P transitions.
+    
+    combined_line = np.zeros(len(nu_shifts))
+    for k in range(2):
+        combined_line += get_natural_absorption_line(k)
+            
+    return combined_line
+
+def get_temperature_spectrum(Temp_He): 
+    #Returns the Doppler-broadened distribution for a given temperature, i.e.
+    # the relative populations of atoms with a corresponding velocity-induced
+    # Doppler shift
+    sig_D_temp = sig_D * np.sqrt(Temp_He)
+    return 1 / np.sqrt(2*np.pi) / sig_D_temp * np.exp(-nu_shifts**2 
+                                                      / (2*sig_D_temp**2))
+
+def get_doppler_broadened_spectrum_complete(Temp_He):
+    #Returns the combined Doppler-broadened scattering cross-section spectrum 
+    # for both 2^3S <--> 2^3P transitions.
+    temp_spectrum = get_temperature_spectrum(Temp_He)
+    combined_spectrum = get_combined_absorption_line()
+    return convolve(combined_spectrum, temp_spectrum)
+
+def g_L_lorentzian(nu_L, Delta_nu_L):
+    #Returns a laser profile with a Lorentzian profile
+    return 2 / np.pi / Delta_nu_L * (Delta_nu_L/2)**2 / ((nu_shifts - nu_L)**2 
+                                                         + (Delta_nu_L/2)**2)
+
+def g_L_gauss(nu_L, Delta_nu_L):
+    #Returns a laser profile with a Gaussian profile
+    sigma_L = Delta_nu_L / 2.355
+    return 1 / sigma_L / np.sqrt(2 * np.pi) * np.exp(-(nu_shifts - nu_L)**2 
+                                                      / (2 * sigma_L**2))
+
+def get_laser_pulseshape(nu_L, Delta_nu_L, lineshape):
+    #Returns a laser profile with the given profile
+    if lineshape == 'gauss':
+        return g_L_gauss(nu_L, Delta_nu_L)
+    elif lineshape == 'lorentzian':
+        return g_L_lorentzian(nu_L, Delta_nu_L)
+    else:
+        print('Invalid lineshape: ' + lineshape)
+        return None
+
+def get_effective_absorption_lines(nu_L=0, Delta_nu_L = 100*10**6,
+                                   lineshape='gauss'):
+    #Returns the effective absorption spectra, accounting for laser lineshape
+    L_jk = np.zeros((2, len(nu_shifts)))
+    laser_spectrum = get_laser_pulseshape(nu_L, Delta_nu_L, lineshape)
+    
+    for k in range(2):
+        alpha_jk = absorb_coeff * g_jk[k] / sum_g * (f_D / np.pi * Delta_nu_n
+                   / 2 / ((nu_shifts + nu_jk[k])**2 + (Delta_nu_n/2)**2))
+        L_jk[k] = convolve(laser_spectrum, alpha_jk)
+    return L_jk
+
+def get_total_scattering_cross_section_spectrum(Temp_He, Delta_nu_L=100e6,
+                                                lineshape='gauss'):
+    #Returns the total effective Doppler-broadened scattering cross-section
+    # spectrum, accounting for the laser lineshape
+    temp_spectrum = get_doppler_broadened_spectrum_complete(Temp_He)
+    laser_spectrum = get_laser_pulseshape(0, Delta_nu_L, lineshape)
+    sigma_tot = convolve(laser_spectrum, temp_spectrum)
+    return sigma_tot
+
+def get_total_scattering_cross_section(Temp_He, nu_L=0, Delta_nu_L=100e6,
+                                       lineshape='gauss'):
+    #Returns the total effective Doppler-broadened scattering cross-section
+    # for a given laser frequency
+    
+    sigma_tot = get_total_scattering_cross_section_spectrum(
+        Temp_He, Delta_nu_L, lineshape)
+    f_sigma = si.interp1d(nu_shifts, sigma_tot)
+    return f_sigma(nu_L)
+
+def fit_wind_and_temp(params, nu_Ls, ydata, Delta_nu_L=100e6,
+                      lineshape='gauss'):
+    #Fit function for a multi-frequency measurement of the scattering
+    # cross-section spectrum, in terms of an amplitude (params[0]),
+    # temperature (params[1]) and line-of-sight wind velocity (params[2])
+    
+    fit = params[0]*get_total_scattering_cross_section(
+        params[1], nu_Ls - params[2]/lamb0, Delta_nu_L, lineshape)
+    return fit - ydata
+
+def N_L_from_pulse_energy(E, nu=nu0): 
+    #Returns the number of photons per pulse for a pulse energy in mJ
+    return E/(h_planck*nu) / 1000
+
+def N_t_laser(nt, delta_t, t_L, N_L):
+    #Returns the number of photons emitted per time interval, assuming the
+    # laser pulse shape
+    # approximation given by VDG
+    
+    t = np.arange(delta_t/2, nt*delta_t + delta_t/2, delta_t)
+    a = 3.4 / t_L 
+    return N_L * t**2 * np.exp(-a*t) * a**3 / 2 * delta_t
+
+def get_saturation_megie(z, alpha_L, t_L, sigma_eff, N_L, T_atm):
+    #Returns the expected degree of saturation, according to the Megie
+    # approach
+    
+    Omega = np.pi / 4 * alpha_L**2    
+    t_s = (z**2 * Omega * t_L) / (2 * sigma_eff * N_L * T_atm)
+    return 1 - 1 / (1 + tau_R/t_s) * (1 - tau_R/t_L * tau_R / (t_s + tau_R)
+                    * (np.exp(-(t_L / tau_R) * (1 + tau_R / t_s))-1))
+
+def get_saturation(nu_L, Delta_nu_L, N_L, z, alpha_L, T_atm, t_L=10, nt=50,
+                   delta_t=1, Temp_He=1000, lineshape='gauss', ratio=False):
+    #Returns the expected degree of saturation, according to the VDG approach    
+    
+    N = N_t_laser(nt, delta_t, t_L, N_L) 
+    Omega = np.pi / 4 * alpha_L**2    
+    temp_spectrum = get_temperature_spectrum(Temp_He)
+    
+    #Find the index for which 99.999% of photons have been accounted for, in
+    # order to abridge the calculation (the DES can be solved analytically for
+    # time steps where the number of photons is approximately zero)
+    
+    try:
+        nt = np.where(np.cumsum(N) > 0.99999 * N_L)[0][0] + 2
+    except IndexError:
+        print('No abridgement possible. Warning:' +  
+              'only {0:.2f} percent of photons accounted for.'.format(
+                  np.sum(N)/N_L * 100))    
+    n = np.zeros((nv, nt))
+    n_e = np.zeros((2, nv, nt))
+    
+    n[:,0] = np.ones(nv)
+    
+    n_e2 = np.zeros((2, nv, nt))
+
+    P_s = 0
+    P_ns = 0
+             
+    
+    L_jk = get_effective_absorption_lines(nu_L, Delta_nu_L, lineshape)
+      
+    for i in range(nt - 1):
+        number_of_photons = T_atm * N[i] / z**2 / Omega
+        f_jk = L_jk * number_of_photons
+        
+        if np.max(f_jk) > 0.1:
+            print('Error: delta_t must be reduced. max_f_jk = {0:.3f}'.format(
+                np.max(f_jk)))
+            return
+          
+        n[:,i+1] = n[:,i] + ((n_e[0,:,i] + n_e[1,:,i]) / tau_R * delta_t
+                                - (n[:,i] - n_e[0,:,i]) * f_jk[0,:] 
+                                - (n[:,i] - 3 / 5 * n_e[1,:,i]) * f_jk[1,:])
+        n_e[0,:,i+1] = n_e[0,:,i] + (-n_e[0,:,i] / tau_R * delta_t
+                                    + (n[:,i] - n_e[0,:,i]) * f_jk[0,:]) 
+        n_e[1,:,i+1] = n_e[1,:,i] + (-n_e[1,:,i] / tau_R * delta_t
+                                  + (n[:,i] - 3 / 5 * n_e[1,:,i]) * f_jk[1,:]) 
+
+        P_s += (np.sum((q_jk[0] * n_e[0,:,i] + q_jk[1] * n_e[1,:,i])
+                      * temp_spectrum) / tau_R * delta_t
+                      / np.sum(temp_spectrum))
+  
+        n_e2[0,:,i+1] = n_e2[0,:,i] + (-n_e2[0,:,i] / tau_R * delta_t
+                                       + f_jk[0,:]) 
+        n_e2[1,:,i+1] = n_e2[1,:,i] + (-n_e2[1,:,i] / tau_R * delta_t
+                                       + f_jk[1,:]) 
+        
+        P_ns += (np.sum((q_jk[0] * n_e2[0,:,i] + q_jk[1] * n_e2[1,:,i]) 
+                        * temp_spectrum) / tau_R * delta_t
+                        / np.sum(temp_spectrum))
+    
+    P_s += (np.sum((q_jk[0] * n_e[0,:,i+1] + q_jk[1] * n_e[1,:,i+1])
+                    * temp_spectrum) / np.sum(temp_spectrum))
+    P_ns += (np.sum((q_jk[0] * n_e2[0,:,i+1] + q_jk[1] * n_e2[1,:,i+1])
+                     * temp_spectrum) / np.sum(temp_spectrum))
+        
+    #If ratio=True, the degree of saturation is returned. If ratio != True,
+    # the total number of emitted photons in the case with saturation and
+    # without saturation are returned individually
+    if ratio:
+        return 1 - P_s / P_ns
+    else:
+        return P_s, P_ns
+
+def gauss_1D(alpha_L, r):
+    #Returns a 1D Gaussian profile
+    return np.exp(-4*r**2/alpha_L**2)
+
+def get_saturation_beam(nu_L, Delta_nu_L, N_L, z, T_atm, alpha_L, alpha_T,
+                        t_L=10, nt=50, delta_t=1, delta_r = 5 * 10**-6,
+                        Temp_He=1000, lineshape='gauss', ratio_beam=True):
+    #Returns the expected degree of saturation, according to the VDG approach,
+    # averaged over a Gaussian beam profile over the field-of-view of the
+    # telescope.
+    
+    #In order to avoid coverage issues, the parameter delta_r is adjusted so
+    # that the field-of-view of the instrument is covered by an integer number
+    # of equal-width bins.
+    r_max = alpha_T/2.
+    n_r = int(r_max / delta_r)+1
+    delta_r_adjusted = r_max / n_r
+    
+    Omega = np.pi / 4 * alpha_L**2
+    
+    r = np.arange(delta_r_adjusted/2, r_max, delta_r_adjusted)
+
+    sats = np.zeros((2,len(r)))
+    beam = N_L * gauss_1D(alpha_L, r)
+    
+    for i in range(len(r)):
+        sats[:,i] = get_saturation(nu_L, Delta_nu_L, beam[i], z, alpha_L,
+                                   T_atm, t_L, nt, delta_t, Temp_He,
+                                   lineshape, ratio=False)
+    
+    #If ratio_beam=True, the degree of saturation is returned. If
+    # ratio_beam != True, the total number of emitted photons in the case
+    # with saturation and without saturation are returned individually
+    if ratio_beam:
+        return 1 - np.sum(sats[0,:] * r) / np.sum(sats[1,:] * r)
+    else:
+        return np.sum(sats[0,:] * r)/Omega, np.sum(sats[1,:] * r)/Omega
+    
+def get_wind_and_temp_errors(Temp_He, nu_Ls, Delta_nu_L, N_L, z, T_atm,
+                             alpha_L, alpha_T, t_L=10, nt=50, delta_t=1,
+                             delta_r=1e-5, lineshape='gauss'):
+    #Determines the retrieved spectrum, with and without saturation, at the
+    # given measurement frequencies. Fits a spectrum based on the measurements
+    # in each case, and returns the fit parameters (amplitude, temperature,
+    # and LOS wind speed)
+    
+    Ps = np.zeros((2,len(nu_Ls)))
+    norm = get_total_scattering_cross_section(Temp_He, 0)
+    
+    for i in range(len(nu_Ls)):
+        nu_L = nu_Ls[i]
+        Ps[:,i] = get_saturation_beam(nu_L, Delta_nu_L, N_L, z, T_atm,
+                                      alpha_L, alpha_T, t_L, nt, delta_t,
+                                      delta_r, Temp_He, lineshape, False)
+    
+    p = np.array([np.max(Ps)/norm, Temp_He, 1])
+    res_sat = opt.leastsq(fit_wind_and_temp, p,
+                          args=(nu_Ls, Ps[0,:], Delta_nu_L, lineshape),
+                          full_output=1)
+    res_no_sat = opt.leastsq(fit_wind_and_temp, p,
+                             args=(nu_Ls, Ps[1,:], Delta_nu_L, lineshape),
+                             full_output=1)
+    
+    return res_sat, res_no_sat, Ps
