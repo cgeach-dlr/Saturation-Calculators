7e258a89237d33ca2e08831fe385b94f95e4c548
cgeach-dlr <christopher.geach@dlr.de>
2026-01-14 09:47:21 +0100

    Update generate_calcium_saturation_plots.py

--- Calcium and calcium plus/generate_calcium_saturation_plots.py
+++ Calcium and calcium plus/generate_calcium_saturation_plots.py
@@ -7,6 +7,7 @@ import calcium_plus_saturation_calculator_library as ca_plus_lib
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+
 import matplotlib
 
 font = {'weight' : 'normal',
@@ -14,25 +15,21 @@ font = {'weight' : 'normal',
 
 matplotlib.rc('font', **font)
 
-c_light = 2.99792458 * 10**8 #m/s
-lamb0 =  422.792 * 10**-9 #m, in vacuum
-nu0 = c_light / lamb0
-
 #Designate an output location for figure data and plots
-outpath_data = ''
-outpath_figs = ''
+outpath = ''
 
 #Calculates the spectra of the degree of saturation for Figures 1 and 3
-      
-nu_Ls = (np.arange(25) - 11)*210e6 #Hz
-lambda_Ls = nu_Ls/nu0 * lamb0 #m
 
-ca_sats_vdG_nu_Ls = np.zeros(len(nu_Ls))
-ca_sats_vdG_nu_Ls2 = np.zeros(len(nu_Ls))
-ca_sats_vdG_nu_Ls3 = np.zeros(len(nu_Ls))
-ca_plus_sats_vdG_nu_Ls = np.zeros(len(nu_Ls))
-ca_plus_sats_vdG_nu_Ls2 = np.zeros(len(nu_Ls))
-ca_plus_sats_vdG_nu_Ls3 = np.zeros(len(nu_Ls))
+lambda_Ls = (np.arange(25) - 11)*0.15e-12
+ca_nu_Ls = lambda_Ls / ca_lib.lamb0 * ca_lib.nu0
+ca_plus_nu_Ls = lambda_Ls / ca_plus_lib.lamb0 * ca_plus_lib.nu0
+
+ca_sats_nu_Ls = np.zeros(len(lambda_Ls))
+ca_sats_nu_Ls2 = np.zeros(len(lambda_Ls))
+ca_sats_nu_Ls3 = np.zeros(len(lambda_Ls))
+ca_plus_sats_nu_Ls = np.zeros(len(lambda_Ls))
+ca_plus_sats_nu_Ls2 = np.zeros(len(lambda_Ls))
+ca_plus_sats_nu_Ls3 = np.zeros(len(lambda_Ls))
 
 ca_E_pulse = 5 #mJ
 ca_N_L = ca_lib.N_L_from_pulse_energy(ca_E_pulse)
@@ -55,62 +52,62 @@ nt = 500
 delta_t = 0.2 #ns
 delta_r = 100e-6 #radians
 
-
 Doppler_spectrum = ca_lib.get_total_scattering_cross_section_spectrum(Temp_Ca)
 
-for i in range(len(nu_Ls)):
-    nu_L = nu_Ls[i]    
-    ca_sats_vdG_nu_Ls[i] = ca_lib.get_saturation_beam(nu_L, ca_Delta_nu_L,
-                ca_N_L, z, T_atm, alpha_L, alpha_T, ca_t_L, nt, delta_t,
-                delta_r, Temp_Ca, 'lorentzian', ratio_beam=True)
-    ca_sats_vdG_nu_Ls2[i] = ca_lib.get_saturation_beam(nu_L, ca_Delta_nu_L/10,
-                ca_N_L, z, T_atm, alpha_L, alpha_T, 10*ca_t_L, nt, 10*delta_t,
-                delta_r, Temp_Ca, 'lorentzian', ratio_beam=True)
-    ca_sats_vdG_nu_Ls3[i] = ca_lib.get_saturation_beam(nu_L, ca_Delta_nu_L/10,
-                ca_N_L, z, T_atm, alpha_L2, alpha_T, 10*ca_t_L, nt, 10*delta_t,
-                delta_r, Temp_Ca, 'lorentzian', ratio_beam=True)    
-    ca_plus_sats_vdG_nu_Ls[i] = ca_plus_lib.get_saturation_beam(nu_L, 
+for i in range(len(lambda_Ls)):
+    ca_sats_nu_Ls[i] = ca_lib.get_saturation_beam(ca_nu_Ls[i],
+                ca_Delta_nu_L, ca_N_L, z, T_atm, alpha_L, alpha_T, ca_t_L, nt,
+                delta_t, delta_r, Temp_Ca, 'lorentzian', ratio_beam=True)
+    ca_sats_nu_Ls2[i] = ca_lib.get_saturation_beam(ca_nu_Ls[i],
+                ca_Delta_nu_L/10, ca_N_L, z, T_atm, alpha_L, alpha_T,
+                10*ca_t_L, nt, 10*delta_t, delta_r, Temp_Ca, 'lorentzian',
+                ratio_beam=True)
+    ca_sats_nu_Ls3[i] = ca_lib.get_saturation_beam(ca_nu_Ls[i],
+                ca_Delta_nu_L/10, ca_N_L, z, T_atm, alpha_L2, alpha_T,
+                10*ca_t_L, nt, 10*delta_t, delta_r, Temp_Ca, 'lorentzian',
+                ratio_beam=True)    
+    ca_plus_sats_nu_Ls[i] = ca_plus_lib.get_saturation_beam(ca_plus_nu_Ls[i], 
                 ca_plus_Delta_nu_L, ca_plus_N_L, z, T_atm, alpha_L, alpha_T,
                 ca_plus_t_L, nt, delta_t, delta_r, Temp_Ca, 'lorentzian',
                 ratio_beam=True)
-    ca_plus_sats_vdG_nu_Ls2[i] = ca_plus_lib.get_saturation_beam(nu_L,
+    ca_plus_sats_nu_Ls2[i] = ca_plus_lib.get_saturation_beam(ca_plus_nu_Ls[i],
                 ca_plus_Delta_nu_L/10, ca_plus_N_L, z, T_atm, alpha_L, alpha_T,
                 10*ca_plus_t_L, nt, 10*delta_t, delta_r, Temp_Ca, 'lorentzian',
                 ratio_beam=True)
-    ca_plus_sats_vdG_nu_Ls3[i] = ca_plus_lib.get_saturation_beam(nu_L,
+    ca_plus_sats_nu_Ls3[i] = ca_plus_lib.get_saturation_beam(ca_plus_nu_Ls[i],
                 ca_plus_Delta_nu_L/10, ca_plus_N_L, z, T_atm, alpha_L2,
                 alpha_T, 10*ca_plus_t_L, nt, 10*delta_t, delta_r, Temp_Ca,
                 'lorentzian', ratio_beam=True)
 
-Spectrum_data = np.vstack((lambda_Ls*1e12, 100*ca_sats_vdG_nu_Ls,
-                   100*ca_sats_vdG_nu_Ls2, 100*ca_sats_vdG_nu_Ls3,
-                   100*ca_plus_sats_vdG_nu_Ls, 100*ca_plus_sats_vdG_nu_Ls2,
-                   100*ca_plus_sats_vdG_nu_Ls3))
+Spectrum_data = np.vstack((lambda_Ls*1e12, 100*ca_sats_nu_Ls, 
+                           100*ca_sats_nu_Ls2, 100*ca_sats_nu_Ls3,
+                           100*ca_plus_sats_nu_Ls, 100*ca_plus_sats_nu_Ls2,
+                           100*ca_plus_sats_nu_Ls3))
 
-np.savetxt(os.path.join(outpath_data, 'Ca_and_ca_plus_spectrum.txt'),
+np.savetxt(os.path.join(outpath, 'Ca_and_ca_plus_spectrum.txt'),
            Spectrum_data.T, delimiter=',')
 
 # Generate plot for Figure 1
 plt.figure(figsize=(12,8))
 
-plt.plot(lambda_Ls*1e12, 100*ca_sats_vdG_nu_Ls, label='Ca')
-plt.plot(lambda_Ls*1e12, 100*ca_plus_sats_vdG_nu_Ls, label='Ca+')
+plt.plot(lambda_Ls*1e12, 100*ca_sats_nu_Ls, label='Ca')
+plt.plot(lambda_Ls*1e12, 100*ca_plus_sats_nu_Ls, label='Ca+')
 plt.legend()
 plt.ylabel('Saturation percent')
 plt.xlabel('Central laser wavelength (pm)')
 
 plt.grid(True)
 
-plt.savefig(os.path.join(outpath_figs, 'Ca_and_ca_plus1.jpg'), dpi=300)
+plt.savefig(os.path.join(outpath, 'Ca_and_ca_plus1.jpg'), dpi=300)
 
 # Generate plot for Figure 3
 plt.figure(figsize=(12,8))
 
-plt.plot(lambda_Ls*1e12, 100*ca_sats_vdG_nu_Ls2, c='tab:blue', label='Ca')
-plt.plot(lambda_Ls*1e12, 100*ca_sats_vdG_nu_Ls3, c='tab:blue', linestyle=':')
-plt.plot(lambda_Ls*1e12, 100*ca_plus_sats_vdG_nu_Ls2, c='tab:orange',
+plt.plot(lambda_Ls*1e12, 100*ca_sats_nu_Ls2, c='tab:blue', label='Ca')
+plt.plot(lambda_Ls*1e12, 100*ca_sats_nu_Ls3, c='tab:blue', linestyle=':')
+plt.plot(lambda_Ls*1e12, 100*ca_plus_sats_nu_Ls2, c='tab:orange',
          label='Ca+')
-plt.plot(lambda_Ls*1e12, 100*ca_plus_sats_vdG_nu_Ls3, c='tab:orange',
+plt.plot(lambda_Ls*1e12, 100*ca_plus_sats_nu_Ls3, c='tab:orange',
          linestyle=':')
 plt.legend()
 plt.ylabel('Saturation percent')
@@ -118,7 +115,7 @@ plt.xlabel('Central laser wavelength (pm)')
 
 plt.grid(True)
 
-plt.savefig(os.path.join(outpath_figs, 'Ca_and_ca_plus3.jpg'), dpi=300)
+plt.savefig(os.path.join(outpath, 'Ca_and_ca_plus3.jpg'), dpi=300)
 
 # Calculates the saturation-induces temperature biases for Figures 2 and 4
 Es = 10**np.arange(0, 2.1, 0.2) #mJ
@@ -133,20 +130,22 @@ ca_plus_T_err_200 = np.zeros(len(Es)) #K
 ca_plus_T_err_200_2 = np.zeros(len(Es)) #K
 ca_plus_T_err_200_3 = np.zeros(len(Es)) #K
 
-nu_Ls_errs = (np.arange(6)-2) * 400e6
+lambda_Ls_errs = (np.arange(6)-2) * 0.24e-12
+ca_nu_Ls_errs = lambda_Ls_errs / ca_lib.lamb0 * ca_lib.nu0
+ca_plus_nu_Ls_errs = lambda_Ls_errs / ca_plus_lib.lamb0 * ca_plus_lib.nu0
 
 for i in range(len(Es)):
     ca_N_L = ca_lib.N_L_from_pulse_energy(Es[i])
-    ca_Res = ca_lib.get_wind_and_temp_errors(Temp_Ca, nu_Ls_errs,
+    ca_Res = ca_lib.get_wind_and_temp_errors(Temp_Ca, ca_nu_Ls_errs,
                                              ca_Delta_nu_L, ca_N_L, z, T_atm,
                                              alpha_L, alpha_T, ca_t_L, nts[i],
                                              100/nts[i], delta_r, 'lorentzian')
-    ca_Res2 = ca_lib.get_wind_and_temp_errors(Temp_Ca, nu_Ls_errs,
+    ca_Res2 = ca_lib.get_wind_and_temp_errors(Temp_Ca, ca_nu_Ls_errs,
                                               ca_Delta_nu_L/10, ca_N_L, z,
                                               T_atm, alpha_L, alpha_T,
                                               10*ca_t_L, nts[i], 1000/nts[i],
                                               delta_r, 'lorentzian')
-    ca_Res3 = ca_lib.get_wind_and_temp_errors(Temp_Ca, nu_Ls_errs,
+    ca_Res3 = ca_lib.get_wind_and_temp_errors(Temp_Ca, ca_nu_Ls_errs,
                                               ca_Delta_nu_L/10, ca_N_L, z,
                                               T_atm, alpha_L2, alpha_T,
                                               10*ca_t_L, nts[i], 1000/nts[i],
@@ -156,21 +155,24 @@ for i in range(len(Es)):
     ca_T_err_200_3[i] = ca_Res3[0][0][1] - ca_Res3[1][0][1]
 
     ca_plus_N_L = ca_plus_lib.N_L_from_pulse_energy(Es[i])
-    ca_plus_Res = ca_plus_lib.get_wind_and_temp_errors(Temp_Ca, nu_Ls_errs,
+    ca_plus_Res = ca_plus_lib.get_wind_and_temp_errors(Temp_Ca, 
+                                                       ca_plus_nu_Ls_errs,
                                                        ca_plus_Delta_nu_L,#
                                                        ca_plus_N_L, z, T_atm,
                                                        alpha_L, alpha_T,
                                                        ca_plus_t_L, nts[i],
                                                        100/nts[i], delta_r,
                                                        'lorentzian')
-    ca_plus_Res2 = ca_plus_lib.get_wind_and_temp_errors(Temp_Ca, nu_Ls_errs,
+    ca_plus_Res2 = ca_plus_lib.get_wind_and_temp_errors(Temp_Ca,
+                                                        ca_plus_nu_Ls_errs,
                                                         ca_plus_Delta_nu_L/10,
                                                         ca_plus_N_L, z, T_atm,
                                                         alpha_L, alpha_T,
                                                         10*ca_plus_t_L, nts[i],
                                                         1000/nts[i], delta_r,
                                                         'lorentzian')
-    ca_plus_Res3 = ca_plus_lib.get_wind_and_temp_errors(Temp_Ca, nu_Ls_errs,
+    ca_plus_Res3 = ca_plus_lib.get_wind_and_temp_errors(Temp_Ca,
+                                                        ca_plus_nu_Ls_errs,
                                                         ca_plus_Delta_nu_L/10,
                                                         ca_plus_N_L, z, T_atm,
                                                         alpha_L2, alpha_T,
@@ -184,7 +186,7 @@ for i in range(len(Es)):
 Temp_bias_data = np.vstack((Es, ca_T_err_200, ca_T_err_200_2, ca_T_err_200_3,
                             ca_plus_T_err_200, ca_plus_T_err_200_2,
                             ca_plus_T_err_200_3))
-np.savetxt(os.path.join(outpath_data, 'Ca_and_ca_plus_temp_biases.txt'),
+np.savetxt(os.path.join(outpath, 'Ca_and_ca_plus_temp_biases.txt'),
            Temp_bias_data.T, delimiter=',')
 
 # Generate plot for Figure 2       
@@ -198,7 +200,7 @@ plt.xlabel('Pulse energy (mJ)')
 
 plt.grid(True)
 
-plt.savefig(os.path.join(outpath_figs, 'Ca_and_ca_plus2.jpg'), dpi=300)
+plt.savefig(os.path.join(outpath, 'Ca_and_ca_plus2.jpg'), dpi=300)
 
 # Generate plot for Figure 4
 plt.figure(figsize=(12,8))
@@ -213,5 +215,4 @@ plt.xlabel('Pulse energy (mJ)')
 
 plt.grid(True)
 
-
-plt.savefig(os.path.join(outpath_figs, 'Ca_and_ca_plus4.jpg'), dpi=300)
+plt.savefig(os.path.join(outpath, 'Ca_and_ca_plus4.jpg'), dpi=300)
