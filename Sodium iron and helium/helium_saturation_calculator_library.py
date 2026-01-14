1b411d52f85ab2911aa965ed334ca01673a338be
cgeach-dlr <christopher.geach@dlr.de>
2026-01-14 10:36:07 +0100

    Update helium_saturation_calculator_library.py

--- Sodium iron and helium/helium_saturation_calculator_library.py
+++ Sodium iron and helium/helium_saturation_calculator_library.py
@@ -71,7 +71,7 @@ def get_natural_absorption_line(k):
 def get_combined_absorption_line():
     #Returns the combined absorption line for both 2^3S <--> 2^3P transitions.
     
-    combined_line = np.zeros(len(nu_shifts))
+    combined_line = np.zeros(nv)
     for k in range(2):
         combined_line += get_natural_absorption_line(k)
             
@@ -130,7 +130,7 @@ def get_effective_absorption_lines(nu_L=0, Delta_nu_L = 100*10**6,
               ' laser line.')
         return
                                            
-    L_jk = np.zeros((2, len(nu_shifts)))
+    L_jk = np.zeros((2, nv))
     laser_spectrum = get_laser_pulseshape(nu_L, Delta_nu_L, lineshape)
     
     for k in range(2):
@@ -332,6 +332,7 @@ def get_wind_and_temp_errors(Temp_He, nu_Ls, Delta_nu_L, N_L, z, T_atm,
                              full_output=1)
     
     return res_sat, res_no_sat, Ps
+
 
 
 
