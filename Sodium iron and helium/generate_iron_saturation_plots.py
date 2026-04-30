beccb6a0747064fedfa4190e65a49f4be39e73c8
cgeach-dlr <christopher.geach@dlr.de>
2026-04-30 08:09:41 +0200

    Refactor iron saturation plot generation code

--- Sodium iron and helium/generate_iron_saturation_plots.py
+++ Sodium iron and helium/generate_iron_saturation_plots.py
@@ -57,9 +57,9 @@ outpath = os.path.join(os.path.dirname(os.getcwd()), 'Output')
 # VDG approaches.
 
 nu_Ls = (np.arange(63) - 31)*1e8 #Hz
-sats_vdG_nu_Ls_gauss = np.zeros(len(nu_Ls))
-sats_vdG_nu_Ls_lorentz = np.zeros(len(nu_Ls))
-sats_Megie_nu_Ls = np.zeros(len(nu_Ls))
+sats_vdG_gauss = np.zeros(len(nu_Ls))
+sats_vdG_lorentz = np.zeros(len(nu_Ls))
+sats_Megie = np.zeros(len(nu_Ls))
 
 line = 2
 E_pulse = 85 #mJ
@@ -80,65 +80,69 @@ Doppler_spectrum = fe_lib.get_total_scattering_cross_section_spectrum(Temp_Fe,
                                                                       line)
 
 for i in range(len(nu_Ls)):
+    print(i)
     nu_L = nu_Ls[i]    
-    sats_vdG_nu_Ls_gauss[i] = fe_lib.get_saturation_beam(line, nu_L,
-                        Delta_nu_L, N_L, z, T_atm, alpha_L, alpha_T, t_L, nt,
-                        delta_t, delta_r, Temp_Fe, 'gauss', ratio_beam=True)    
-    sats_vdG_nu_Ls_lorentz[i] = fe_lib.get_saturation_beam(line, nu_L, 
-                      Delta_nu_L, N_L, z, T_atm, alpha_L, alpha_T, t_L, nt,
-                      delta_t, delta_r, Temp_Fe, 'lorentzian',
-                      ratio_beam=True)    
+    sats_vdG_gauss[i] = fe_lib.get_saturation_beam(line, nu_L, Delta_nu_L, N_L,
+                        z, T_atm, alpha_L, alpha_T, t_L, nt, delta_t, delta_r,
+                        Temp_Fe, 'gauss', ratio_beam=True)    
+    sats_vdG_lorentz[i] = fe_lib.get_saturation_beam(line, nu_L, Delta_nu_L,
+                        N_L, z, T_atm, alpha_L, alpha_T, t_L, nt, delta_t,
+                        delta_r, Temp_Fe, 'lorentzian', ratio_beam=True)    
     g_L = fe_lib.get_laser_pulseshape(nu_L, Delta_nu_L, lineshape='gauss')
     sigma_eff = np.sum(g_L * Doppler_spectrum) / np.sum(g_L)
-    sats_Megie_nu_Ls[i] = fe_lib.get_saturation_megie(z, alpha_L, t_L,
-                    sigma_eff, N_L, T_atm, line)
-
+    sats_Megie[i] = fe_lib.get_saturation_megie(z, alpha_L, t_L, sigma_eff, N_L,
+                                                T_atm, line)
+    
 #Calculates the saturation-induced density and temperature biases as a
 # function of laser pulse energy
 
 nu_Ls_errs = (np.arange(16) - 7.5) * 43e7 #Hz
-line = 2
+line = 2 #Selects the 386 nm resonance line)
 
-Es = 10**np.arange(0, 2.1, 0.2) #mJ
+Es = 1.075 * 10**np.arange(0, 2.1, 0.2) #mJ (The factor 1.075 scales the 
+                                        # maxiumum pulse energy such that the 
+                                        # energy density is 1 mJ / m^2)
 Es = np.hstack((1e-3, Es))
+
+Omega = np.pi / 4 * np.sin(alpha_L)**2
+u_0s = Es / z**2 / Omega
 
 delta_ts = np.array([10, 10, 10, 10, 10, 10, 10, 10, 5, 5, 2, 2])
 nts = 1250 / delta_ts
 nts = nts.astype(np.int16)
 
-dens_err_200_gauss = np.zeros(len(Es))
-T_err_200_gauss = np.zeros(len(Es))
-w_err_200_gauss = np.zeros(len(Es))
-dens_err_200_lorentz = np.zeros(len(Es))
-T_err_200_lorentz = np.zeros(len(Es))
-w_err_200_lorentz = np.zeros(len(Es))
+dens_err_gauss = np.zeros(len(Es))
+T_err_gauss = np.zeros(len(Es))
+dens_err_lorentz = np.zeros(len(Es))
+T_err_lorentz = np.zeros(len(Es))
 
 for i in range(len(Es)):
+    print(i)
     N_L = fe_lib.N_L_from_pulse_energy(Es[i], line)
-    Res_gauss = fe_lib.get_wind_and_temp_errors(line, 200, nu_Ls_errs,
+    Res_gauss = fe_lib.get_wind_and_temp_errors(line, Temp_Fe, nu_Ls_errs,
                                 Delta_nu_L, N_L, z, T_atm, alpha_L, alpha_T,
                                 t_L, nts[i], delta_ts[i], delta_r, 'gauss')
-    Res_lorentz = fe_lib.get_wind_and_temp_errors(line, 200, nu_Ls_errs,
+    Res_lorentz = fe_lib.get_wind_and_temp_errors(line, Temp_Fe, nu_Ls_errs,
                                 Delta_nu_L, N_L, z, T_atm, alpha_L, alpha_T,
                                 t_L, nts[i], delta_ts[i], delta_r,
                                 'lorentzian')
       
-    dens_err_200_gauss[i] = ((Res_gauss[0][0][0] - Res_gauss[1][0][0])
-                              / Res_gauss[1][0][0])
-    T_err_200_gauss[i] = Res_gauss[0][0][1] - Res_gauss[1][0][1]
+    dens_err_gauss[i] = ((Res_gauss[0][0][0] - Res_gauss[1][0][0]) 
+                             / Res_gauss[1][0][0])
+    T_err_gauss[i] = Res_gauss[0][0][1] - Res_gauss[1][0][1]
     
-    dens_err_200_lorentz[i] = ((Res_lorentz[0][0][0] - Res_lorentz[1][0][0])
+    dens_err_lorentz[i] = ((Res_lorentz[0][0][0] - Res_lorentz[1][0][0])
                                 / Res_lorentz[1][0][0])
-    T_err_200_lorentz[i] = Res_lorentz[0][0][1] - Res_lorentz[1][0][1]
+    T_err_lorentz[i] = Res_lorentz[0][0][1] - Res_lorentz[1][0][1]
 
 fig,ax = plt.subplots(1,2, figsize=(16,8))
 
-ax[0].plot(nu_Ls*1e-9, 100*sats_vdG_nu_Ls_gauss, label='von der Gathen ' + 
-           'approach (Gauss profile)')
-ax[0].plot(nu_Ls*1e-9, 100*sats_vdG_nu_Ls_lorentz, label='von der Gathen ' +
-           'approach (Lorentz profile)')
-ax[0].plot(nu_Ls*1e-9, 100*sats_Megie_nu_Ls, label='Megie approach ' + 
-           r'($\sigma_{\mathrm{t}}$)')
+ax[0].plot(nu_Ls*1e-9, 100*sats_vdG_gauss,
+           label='von der Gathen approach (Gauss profile)')
+ax[0].plot(nu_Ls*1e-9, 100*sats_vdG_lorentz,
+           label='von der Gathen approach (Lorentz profile)')
+ax[0].plot(nu_Ls*1e-9, 100*sats_Megie,
+           label=r'Megie approach ($\sigma_{\mathrm{t}}$)')
 
 ax[0].text(.06,.92, '(a)', transform=plt.gcf().transFigure)
 ax[0].set_ylabel('Saturation percent')
@@ -148,20 +152,19 @@ ax[0].legend(fontsize=16)
 ax[0].grid(True)
 
 ax[1].text(.51,.93, '(b)', transform=plt.gcf().transFigure)
-a0, = ax[1].plot(Es, dens_err_200_gauss, label = 'Density error')
-a1, = ax[1].plot(Es, dens_err_200_lorentz, c='tab:blue', linestyle='--',
-           label = 'Density error (Lorentz profile)')
-a2, =ax[1].plot(Es, T_err_200_gauss, label = 'Temperature error')
-a3, =ax[1].plot(Es, T_err_200_lorentz, c='tab:orange', linestyle='--',
-           label = 'Wind error (Lorentz profile)')
-ax[1].set_ylim(-0.4,1)
+a0, = ax[1].plot(u_0s, dens_err_gauss)
+a1, = ax[1].plot(u_0s, dens_err_lorentz, c='tab:blue', linestyle='--')
+a2, = ax[1].plot(u_0s, T_err_gauss)
+a3, = ax[1].plot(u_0s, T_err_lorentz, c='tab:orange', linestyle='--')
+ax[1].axvline(E_pulse / z**2 / Omega * T_atm, ls='--', c='k')
+ax[1].set_ylim(-0.4,1.1)
 
-ax[1].set_xlabel('Laser pulse energy (mJ)')
-ax[1].set_ylabel('Fractional density error \n Absolute temperature error (K)')
+ax[1].set_xlabel(r'Laser energy density (mJ/m$^2$)')
+ax[1].set_ylabel('Fractional density bias \n Absolute temperature bias (K)')
 
 ax[1].legend([(a0, a1), (a2,a3)],
-             ['Density error (gauss/lorentzian profile)',
-              'Temperature error (gauss/lorentzian profile)'],
+             ['Density bias (Gauss/Lorentz profile)',
+              'Temperature bias (Gauss/Lorentz profile)'],
            handler_map = {tuple : HandlerTupleVertical()}, fontsize=16)
 
 ax[1].grid(True)
@@ -171,17 +174,13 @@ fig.tight_layout()
 
 plt.savefig(os.path.join(outpath, 'Fe.pdf'), dpi=300)
 
-Fe_spectrum_data = np.vstack((nu_Ls*1e-9, 100*sats_vdG_nu_Ls_gauss,
-                              100*sats_vdG_nu_Ls_lorentz,
-                              100*sats_Megie_nu_Ls))
+Fe_spectrum_data = np.vstack((nu_Ls*1e-9, 100*sats_vdG_gauss,
+                              100*sats_vdG_lorentz, 100*sats_Megie))
 np.savetxt(os.path.join(outpath, 'Fe_saturation.txt'),
            Fe_spectrum_data.T, delimiter=',')
 
-Fe_temp_and_wind_biases_data = np.vstack((Es, dens_err_200_gauss,
-                                          dens_err_200_lorentz,
-                                          T_err_200_gauss,
-                                          T_err_200_lorentz))
+Fe_temp_and_wind_biases_data = np.vstack((u_0s, dens_err_gauss,
+                                          dens_err_lorentz, T_err_gauss,
+                                          T_err_lorentz))
 np.savetxt(os.path.join(outpath, 'Fe_temp_and_wind_biases.txt'),
            Fe_temp_and_wind_biases_data.T, delimiter=',')
-
-
