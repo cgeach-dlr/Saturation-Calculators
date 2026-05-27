a0ac31ff7bc195fa525a0e1f30523e973a568f12
cgeach-dlr <christopher.geach@dlr.de>
2026-05-27 09:20:36 +0200

    Refactor plot_sodium_data_SI.py for clarity

--- Sodium iron and helium/plot_sodium_data_SI.py
+++ Sodium iron and helium/plot_sodium_data_SI.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 25 08:22:33 2026
-
-@author: geac_ch
-"""
 
 import sys
 sys.path.append("")
@@ -43,42 +38,6 @@ z = 90000 #m
 alpha_L = 450e-6 #rad
 Omega = np.pi / 4 * np.sin(alpha_L)**2
 T_atm = 0.7 
-
-fig,ax = plt.subplots(1,2, figsize=(16,8))
-
-ax[0].plot(nu_Ls, sats_vdG_corr,
-           label='VDG approach\n (corrected line strengths)')
-ax[0].plot(nu_Ls, sats_vdG_uncorr,
-           label='VDG approach\n (uncorrected line strengths)')
-ax[0].plot(nu_Ls, sats_Megie, label=r'Megie approach ($\sigma_{\mathrm{t}}$)')
-
-ax[0].text(.06,.92, '(a)', transform=plt.gcf().transFigure)
-ax[0].set_ylabel('Saturation percent')
-ax[0].set_xlabel('Laser frequency offset (GHz)')
-ax[0].set_xlim(-3.5,3.5)
-ax[0].set_ylim(-0.5,8.9)
-ax[0].legend()
-ax[0].grid(True)
-
-ax[1].text(.51,.93, '(b)', transform=plt.gcf().transFigure)
-ax[1].plot(u_0s, dens_err, label='Density bias')
-ax[1].plot(u_0s, T_err, label='Temperature bias')
-ax[1].plot(u_0s, w_err, label='Wind bias')
-ax[1].axvline(E_pulse / z**2 / Omega * T_atm, ls='--', c='k')
-
-ax[1].set_ylim(-0.5,12.5)
-
-ax[1].set_xlabel(r'Laser energy density (mJ/m$^2$)')
-ax[1].set_ylabel('Fractional density bias \n Absolute temperature bias ' + 
-                 '(K)\n Absolute wind bias (m/s)')
-ax[1].grid(True)
-ax[1].yaxis.set_label_position("right")
-ax[1].yaxis.tick_right()
-ax[1].legend(loc='upper left', framealpha=1)
-
-fig.tight_layout()
-
-plt.savefig(os.path.join(outpath, 'Na_SI.pdf'), dpi=300)
 
 fig,ax = plt.subplots(1,2, figsize=(16,8))
 
